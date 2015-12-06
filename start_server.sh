b783619b42d9154ebe41212a4c137cd69368edb4
James Swineson <jamesswineson@gmail.com>
2015-12-06 22:56:25 +0800

    use `printf` instead of `echo` in start script to prevent trailing new line

--- start_server.sh
+++ start_server.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'dst_server' ]; then
     mkdir -p $DST_DATA_DIR/DoNotStarveTogether
     if [ ! -f $DST_DATA_DIR/DoNotStarveTogether/server_token.txt ]; then
-        echo $DST_SERVER_TOKEN > $DST_DATA_DIR/DoNotStarveTogether/server_token.txt
+        printf $DST_SERVER_TOKEN > $DST_DATA_DIR/DoNotStarveTogether/server_token.txt
     fi
     
     cd $DST_INSTALLATION_DIR/bin
