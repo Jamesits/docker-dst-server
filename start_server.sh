58094a9c60b70f5000b245e771c91f644d8c532c
James Swineson <jamesswineson@gmail.com>
2015-12-06 17:49:29 +0800

    add server token creation in startup script

--- start_server.sh
+++ start_server.sh
@@ -3,6 +3,10 @@ set -e
 
 if [ "$1" = 'dst_server' ]; then
     mkdir -p $DST_DATA_DIR/DoNotStarveTogether
+    if [ ! -f $DST_DATA_DIR/DoNotStarveTogether/server_token.txt ]; then
+        echo $DST_SERVER_TOKEN > $DST_DATA_DIR/DoNotStarveTogether/server_token.txt
+    fi
+    
     cd $DST_INSTALLATION_DIR/bin
     exec gosu ./dontstarve_dedicated_server_nullrenderer -port $PORT -persistent_storage_root $DST_DATA_DIR "$@"
 fi
