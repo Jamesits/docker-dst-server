0e1852e6b6819d4d17fbae835dca3ec707cc1ba4
James Swineson <jamesswineson@gmail.com>
2015-12-20 18:08:50 +0800

    do not split SteamCMD command into multiple lines

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -33,11 +33,7 @@ Github: https://github.com/Jamesits/Don-t-Starve-Together-Dedicated-Server
 
 if [ "$1" = 'start' ]; then
     echo >&2 "Updating server..."
-    "$STEAMCMD_INSTALLATION_DIR"/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1
-        +login anonymous \
-        +force_install_dir "$DST_INSTALLATION_DIR" \
-        +app_update 343050 validate \
-        +quit
+    "$STEAMCMD_INSTALLATION_DIR"/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir "$DST_INSTALLATION_DIR" +app_update 343050 validate +quit
     cat >&2 /root/Steam/logs/stderr.txt
     
     echo >&2 "Checking server token..."
