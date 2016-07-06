535ba3d645143c034d0445f05f5f7c1f83a8c5bb
James Swineson <jamesswineson@gmail.com>
2016-07-07 00:03:13 +0800

    Refactoring, removed unused code

--- Dockerfile
+++ Dockerfile
@@ -1,54 +1,32 @@
-# Don't Starve Together Dedicated Server Docker Image
-#Copyright (C) 2015 James Swineson (Jamesits)
-#Copyright (C) 2015 Mingye Wang (Arthur2e5)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 FROM debian:latest
-MAINTAINER James Swineson "jamesswineson@gmail.com"
-
-ENV STEAMCMD_INSTALLATION_DIR=/usr/local/src/steamcmd \
-	DST_INSTALLATION_DIR=/usr/local/src/dst_server/ \
-	DST_DATA_DIR=/data \
-	DST_PORT=10999
+MAINTAINER James Swineson <jamesswineson@gmail.com>
 
 ENV DEBIAN_FRONTEND noninteractive
 
 RUN dpkg --add-architecture i386 \
- 	&&apt-get update -y && apt-get install -y \
-		lib32gcc1 \
-		lib32stdc++6 \
-		libcurl4-gnutls-dev:i386 \
-		wget \
-		tar \
+ 	&& apt-get update -y \
+    && apt-get upgrade -y \
+    && apt-get install -y lib32gcc1 lib32stdc++6 libcurl4-gnutls-dev:i386 wget tar \
  	&& apt-get clean \
  	&& rm -rf /var/lib/apt/lists/*
-	 
-RUN mkdir -p "$STEAMCMD_INSTALLATION_DIR" \
-	&& wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz -O /tmp/steamcmd.tar.gz \
-	&& tar -xvzf /tmp/steamcmd.tar.gz -C "$STEAMCMD_INSTALLATION_DIR"
-	
-RUN mkdir -p "$DST_INSTALLATION_DIR" \
-	&& $STEAMCMD_INSTALLATION_DIR/steamcmd.sh +login anonymous +force_install_dir $DST_INSTALLATION_DIR +app_update 343050 validate +quit \
-	&& cat /root/Steam/logs/stderr.txt \
-	&& mkdir -p "$DST_DATA_DIR"/DoNotStarveTogether \
-	&& cp "$DST_INSTALLATION_DIR"/mods/dedicated_server_mods_setup.lua "$DST_DATA_DIR"/DoNotStarveTogether/
 
-COPY ./docker-entrypoint.sh "$DST_DATA_DIR"/docker-entrypoint.sh
-RUN chmod a+x "$DST_DATA_DIR"/docker-entrypoint.sh
-	
-ENTRYPOINT [ "/data/docker-entrypoint.sh" ]
-CMD [ "start" ]
+RUN mkdir -p /usr/local/src/steamcmd \
+	&& wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz -O /tmp/steamcmd.tar.gz \
+	&& tar -xvzf /tmp/steamcmd.tar.gz -C /usr/local/src/steamcmd
+    
+RUN mkdir -p /data/dst
+VOLUME /data/dst
+
+RUN mkdir -p /usr/local/src/dst_server \
+	&& /usr/local/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir "/usr/local/src/dst_server" +app_update 343050 validate +quit \
+	&& cat /root/Steam/logs/stderr.txt \
+    && ln -s /usr/local/src/dst_server/bin/dontstarve_dedicated_server_nullrenderer /usr/local/bin/dst-server
+
+WORKDIR /usr/local/src/dst_server/bin
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+COPY config/* /data/dst
+
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
+CMD [ "dst-server", "-port", "10999", "-persistent_storage_root", "/data", "-conf_dir", "dst" ]
 EXPOSE 10999/udp
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,125 +1,33 @@
-#!/bin/sh -e
-
-# Don't Starve Together Dedicated Server Docker Image
-# Copyright (C) 2015 James Swineson (Jamesits) <jamesswineson@gmail.com>
-# Copyright (C) 2015 Mingye Wang (Arthur2e5) <arthur2e5@aosc.io>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-printf '%s\n' "
-Don't Starve Together Dedicated Server Docker Startup Image, 
-Copyright (C) 2015 James Swineson (Jamesits)
-Copyright (C) 2015 Mingye Wang (Arthur2e5)
-This script comes with ABSOLUTELY NO WARRANTY. This is free software, and you 
-are welcome to redistribute it under certain conditions;
-visit https://github.com/Jamesits/Don-t-Starve-Together-Dedicated-Server/blob/master/LICENSE
-for details.
-
-Docker Hub: https://hub.docker.com/r/jamesits/don-t-starve-together-dedicated-server/
-Github: https://github.com/Jamesits/Don-t-Starve-Together-Dedicated-Server
-"
+#!/bin/bash -e
 
 ##set -e error handler.
 on_error() {
   echo >&2 "Error on line ${1}${3+: ${3}}; RET ${2}."
-  echo "This error is triggered by \`sh -e' in the script; to override it, run `sh "$0"` instead."
   exit $2
 }
 trap 'on_error ${LINENO} $?' ERR 2>/dev/null || true # some shells don't have ERR trap.
 
-##runs the specified steam cmds.
-steam_eval(){
-    _ret=0
-    #>args.EACH {|cmd| -> steamcmd_args += shell_eval_expd("+#{cmd}")}
-    #-array steamcmd_args as new args. inplace.
-    for eval_cmd; do
-        eval 'set -$- -- "$@" '"+$eval_cmd"
-    done
-    "$STEAMCMD_INSTALLATION_DIR"/steamcmd.sh \
-        +@ShutdownOnFailedCommand 1 \
-        +@NoPromptForPassword 1 \
-        +login anonymous \
-        +force_install_dir "$DST_INSTALLATION_DIR" "$@" +quit || _ret=$?
-    cat >&2 /root/Steam/logs/stderr.txt || true
-    return $_ret
-}
-
-##updates the server.
-steam_update_dst(){
-    echo >&2 "Updating server..."
-    steam_eval 'app_update 343050 validate'
-}
-
-##Calls the DST binary in the right dir.
-dst_bin(){
-    pushd "$DST_INSTALLATION_DIR"/bin
-    ./dontstarve_dedicated_server_nullrenderer "$@"
-    popd
-}
-
-##Execs to the DST binary in the right dir.
-exec_dst_bin(){
-    cd "$DST_INSTALLATION_DIR"/bin
-    exec ./dontstarve_dedicated_server_nullrenderer "$@"
-}
-
-##shorthand for a bunch of cp magic
-copy(){ cp -a --reflink=auto "$@"; }
-
-case "$1" in
-start)
+if [ "$1" -e "dst-server" ]; then
     shift
-    steam_update_dst
-    echo >&2 "Checking server token..."
-    mkdir -p "$DST_DATA_DIR"/DoNotStarveTogether
-    if [ ! -f "$DST_DATA_DIR"/DoNotStarveTogether/server_token.txt ]; then
-        printf '%s\0' "$DST_SERVER_TOKEN" > "$DST_DATA_DIR"/DoNotStarveTogether/server_token.txt
+    # Update game
+    /usr/local/src/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir "/usr/local/src/dst_server" +app_update 343050 +validate +quit
+    dst-server -only_update_server_mods
+    
+    echo "Checking server token..."
+    if [ ! -f /data/dst/server_token.txt ]; then
+        printf '%s\0' "$DST_SERVER_TOKEN" > /data/dst/server_token.txt
     fi
     
-    echo >&2 "Applying server mod settings..."
-    if [ -f "$DST_DATA_DIR"/DoNotStarveTogether/dedicated_server_mods_setup.lua ]; then
-        copy "$DST_DATA_DIR"/DoNotStarveTogether/dedicated_server_mods_setup.lua "$DST_INSTALLATION_DIR"/mods/
+    echo "Applying server mod settings..."
+    if [ -f /data/dst/dedicated_server_mods_setup.lua ]; then
+        cp /data/dst/dedicated_server_mods_setup.lua /usr/local/src/dst_server/mods/
     else
-        copy "$DST_INSTALLATION_DIR"/mods/dedicated_server_mods_setup.lua "$DST_DATA_DIR"/DoNotStarveTogether/
+        cp /usr/local/src/dst_server/mods/dedicated_server_mods_setup.lua /data/dst/
     fi
     
-    echo >&2 "Applying user settings..."
-    mkdir -p "$DST_DATA_DIR"/DoNotStarveTogether/save/
-    copy "$DST_DATA_DIR"/DoNotStarveTogether/*list.txt "$DST_DATA_DIR"/DoNotStarveTogether/save/ || true
-    
-    echo >&2 "Starting server..."
-    exec_dst_bin -port "$DST_PORT" -persistent_storage_root "$DST_DATA_DIR" "$@"
-    ;;
+    echo "Applying user settings..."
+    mkdir -p /data/dst/save/
+    copy /data/dst/*list.txt /data/dst/save/ || true
+fi
 
-reset)
-    echo >&2 "Saving user settings..."
-    copy "$DST_DATA_DIR"/DoNotStarveTogether/save/*list.txt "$DST_DATA_DIR"/DoNotStarveTogether/ || true
-    
-    echo >&2 "Deleting saved game..."
-    rm -rf "$DST_DATA_DIR"/DoNotStarveTogether/save
-    echo >&2 "Saved game deleted successfully."
-    ;;
-
-update)
-    steam_update_dst    
-    echo >&2 "Updating server mods..."
-    dst_bin -only_update_server_mods
-    echo >&2 "Server updated successfully."
-    ;;
-
-*)
-    exec "$@"
-    ;;
-esac
+exec "$@"
