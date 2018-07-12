e17f30ef9d42c140c8e4e71ba62439b236c964de
James Swineson <github@public.swineson.me>
2018-07-12 23:12:55 +0800

    cleanup entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,32 +8,35 @@ on_error() {
 }
 trap 'on_error ${LINENO} $?' ERR 2>/dev/null || true # some shells don't have ERR trap.
 
-# create default server config if there is none
-if [ ! -d "/data/DoNotStarveTogether" ]; then
-    echo "Creating default server config..."
-    cp -r /opt/dst_default_config/* /data
-fi
-
-# override server mods folder with user provided one
-if [ ! -d "/data/DoNotStarveTogether/Cluster_1/mods" ]; then
-    echo "Creating default mod config..."
-    mkdir -p /data/DoNotStarveTogether/Cluster_1
-    cp -r /opt/dst_server/mods /data/DoNotStarveTogether/Cluster_1
-fi
-rm -rf /opt/dst_server/mods
-ln -s /data/DoNotStarveTogether/Cluster_1/mods /opt/dst_server/mods
-
-# check cluster token
-if [ ! -f "/data/DoNotStarveTogether/Cluster_1/cluster_token.txt" ]; then
-    >&2 echo "cluster_token.txt not found!"
-else
-    # the cluster_token.txt needs to be terminated without newline, try to fix
-    mv /data/DoNotStarveTogether/Cluster_1/cluster_token.txt /tmp/
-    tr -d '\n' < /tmp/cluster_token.txt > /data/DoNotStarveTogether/Cluster_1/cluster_token.txt
-    rm -f /tmp/cluster_token.txt
-fi
-
 if [ "$1" == "dontstarve_dedicated_server_nullrenderer" -o "$1" == "supervisord" ]; then
+    # create default server config if there is none
+    if [ ! -d "/data/DoNotStarveTogether" ]; then
+        echo "Creating default server config..."
+        cp -r /opt/dst_default_config/* /data
+        touch /data/DoNotStarveTogether/Cluster_1/cluster_token.txt
+        echo "Done, please fill in `DoNotStarveTogether/Cluster_1/cluster_token.txt` with your cluster token and restart server!"
+        exit
+    fi
+
+    # override server mods folder with user provided one
+    if [ ! -d "/data/DoNotStarveTogether/Cluster_1/mods" ]; then
+        echo "Creating default mod config..."
+        mkdir -p /data/DoNotStarveTogether/Cluster_1
+        cp -r /opt/dst_server/mods /data/DoNotStarveTogether/Cluster_1
+    fi
+    rm -rf /opt/dst_server/mods
+    ln -s /data/DoNotStarveTogether/Cluster_1/mods /opt/dst_server/mods
+
+    # check cluster token
+    if [ ! -f "/data/DoNotStarveTogether/Cluster_1/cluster_token.txt" ]; then
+        >&2 echo "cluster_token.txt not found!"
+    else
+        # the cluster_token.txt needs to be terminated without newline, try to fix
+        mv /data/DoNotStarveTogether/Cluster_1/cluster_token.txt /tmp/
+        tr -d '\n' < /tmp/cluster_token.txt > /data/DoNotStarveTogether/Cluster_1/cluster_token.txt
+        rm -f /tmp/cluster_token.txt
+    fi
+
     # Update game
     echo "Updating server..."
     steamcmd +runscript /opt/steamcmd_scripts/install_dst_server
