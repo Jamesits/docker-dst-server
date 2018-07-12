7becb2d7d2cfa409f3bef66b1dabfe5beeef09ee
James Swineson <github@public.swineson.me>
2018-07-12 21:17:48 +0800

    if mods folder is not in user config, create it

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,11 @@ on_error() {
 trap 'on_error ${LINENO} $?' ERR 2>/dev/null || true # some shells don't have ERR trap.
 
 # override server mods folder with user provided one
-if [ -d "/data/DoNotStarveTogether/Cluster_1/mods" ]; then
-    mount --bind /data/DoNotStarveTogether/Cluster_1/mods /opt/dst_server/mods
+if [ ! -d "/data/DoNotStarveTogether/Cluster_1/mods" ]; then
+    mkdir -p /data/DoNotStarveTogether/Cluster_1
+    cp -r /opt/dst_server/mods /data/DoNotStarveTogether/Cluster_1
 fi
+mount --bind /data/DoNotStarveTogether/Cluster_1/mods /opt/dst_server/mods
 
 if [ "$1" == "dontstarve_dedicated_server_nullrenderer" -o "$1" == "supervisord" ]; then
     # Update game
