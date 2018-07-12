55507615e27938319c25af66e76bdecded777932
James Swineson <github@public.swineson.me>
2018-07-12 20:55:23 +0800

    process server mods installation

---
+++ default-config/DoNotStarveTogether/Cluster_1/mods/modsettings.lua
@@ -0,0 +1,17 @@
+-- Use the "ForceEnableMod" function when developing a mod. This will cause the
+-- game to load the mod every time no matter what, saving you the trouble of
+-- re-enabling it from the main menu.
+--
+-- Note! You shout NOT do this for normal mod loading. Please use the Mods menu
+-- from the main screen instead.
+
+--ForceEnableMod("kioskmode_dst")
+
+
+-- Use "EnableModDebugPrint()" to show extra information during startup.
+
+--EnableModDebugPrint()
+
+-- Use "EnableModError()" to make the game more strict and crash on bad mod practices.
+
+--EnableModError()
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -eu
 
-##set -e error handler.
+# set -e error handler.
 on_error() {
-  echo >&2 "Error on line ${1}${3+: ${3}}; RET ${2}."
-  exit $2
+    echo >&2 "Error on line ${1}${3+: ${3}}; RET ${2}."
+    exit $2
 }
 trap 'on_error ${LINENO} $?' ERR 2>/dev/null || true # some shells don't have ERR trap.
+
+# override server mods folder with user provided one
+if [ -d "/data/DoNotStarveTogether/Cluster_1/mods" ]; then
+    mount --bind /data/DoNotStarveTogether/Cluster_1/mods /opt/dst_server/mods
+fi
 
 if [ "$1" == "dontstarve_dedicated_server_nullrenderer" -o "$1" == "supervisord" ]; then
     # Update game
