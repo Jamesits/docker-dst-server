2c40577eb4f09533f9e3b07febcf485a7d339ec3
James Swineson <github@public.swineson.me>
2020-03-27 21:49:08 -0700

    Refactor to allow this image to base on a variaty of different images
    Supported bases:
    * Debian 10
    * Ubuntu (eoan or later)
    * deb-based images with steamcmd pre-installed
    
    Also added DST_CLUSTER_TOKEN environment variable

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,41 @@
-FROM debian:buster-slim
+ARG BASE_IMAGE=debian:buster-slim
+FROM $BASE_IMAGE
 LABEL maintainer="James Swineson <docker@public.swineson.me>"
 
 ARG DEBIAN_FRONTEND=noninteractive
 ARG STEAMCMD_URL=https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+ARG STEAMCMD_PATH=/usr/local/bin/steamcmd
+ARG DST_DOWNLOAD=1
 ARG LANG=C.UTF-8
 ARG LC_ALL=C.UTF-8
+ARG DST_USER=dst
+ARG DST_GROUP=dst
+ARG DST_USER_DATA_PATH=/data
 
 # install packages
 RUN dpkg --add-architecture i386 \
     && apt-get update -y \
-    && apt-get install -y --no-install-recommends ca-certificates lib32gcc1 lib32stdc++6 libcurl4-gnutls-dev:i386 wget tar supervisor \
+    && apt-get install -y --no-install-recommends ca-certificates lib32gcc1 lib32stdc++6 libcurl3-gnutls:i386 wget tar supervisor \
     && apt-get autoremove -y \
     && apt-get clean -y \
     && rm -rf /var/lib/apt/lists/*
 
     # create data directory
-RUN mkdir -p /data \
+RUN mkdir -p "${DST_USER_DATA_PATH}" \
     # Add unprivileged user
-    && groupadd dst \
-    && useradd -g dst -d /data dst \
-    && chown -R dst:dst /data
+    && ( groupadd "${DST_GROUP}" || true ) \
+    && ( useradd -g "${DST_GROUP}" -d "${DST_USER_DATA_PATH}" "${DST_USER}" || true )\
+    && chown -R "${DST_USER}:${DST_GROUP}" "${DST_USER_DATA_PATH}"
 
-# install steamcmd
-RUN mkdir -p /opt/steamcmd \
+# install steamcmd only if steamcmd doesn't exist
+RUN test -e "${STEAMCMD_PATH}" && echo "Steamcmd detected" \
+    || ( \
+    echo "Installing steamcmd" \
+    && mkdir -p /opt/steamcmd \
     && wget "${STEAMCMD_URL}" -O /tmp/steamcmd.tar.gz \
     && tar -xvzf /tmp/steamcmd.tar.gz -C /opt/steamcmd \
-    && rm -rf /tmp/*
+    && rm -rf /tmp/* \
+    )
 
 # install helper tools
 COPY supervisor /etc/supervisor/
@@ -34,15 +44,20 @@ COPY scripts_steam /opt/steamcmd_scripts/
 RUN chmod +x /usr/local/bin/*
 
 # install Don't Starve Together server
-RUN mkdir -p /opt/dst_server \
-    && steamcmd +runscript /opt/steamcmd_scripts/install_dst_server_initial \
+RUN mkdir -p /opt/dst_server && chmod a+rw /opt/dst_server \
+    && ( test "${DST_DOWNLOAD}" = "1" && steamcmd +runscript /opt/steamcmd_scripts/install_dst_server_initial || true ) \
     && rm -rf /root/Steam /root/.steam
 
 # install default config
 COPY dst_default_config /opt/dst_default_config/
-RUN chown -R dst:dst /opt/dst_default_config
+RUN chown -R "${DST_USER}:${DST_GROUP}" /opt/dst_default_config
 
-VOLUME [ "/data" ]
+# pass essential environment variables into the container
+ENV STEAMCMD_PATH=$STEAMCMD_PATH
+ENV DST_USER=$DST_USER
+ENV DST_GROUP=$DST_GROUP
+ENV DST_USER_DATA_PATH=$DST_USER_DATA_PATH
+VOLUME [ "${DST_USER_DATA_PATH}" ]
 
 EXPOSE 10999-11000/udp 12346-12347/udp
 ENTRYPOINT [ "entrypoint.sh" ]
--- scripts_system/entrypoint.sh
+++ scripts_system/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -Eeuo pipefail
 
 # set -e error handler.
 on_error() {
@@ -10,44 +10,49 @@ trap 'on_error ${LINENO} $?' ERR 2>/dev/null || true # some shells don't have ER
 
 if [ "$1" == "dontstarve_dedicated_server_nullrenderer" -o "$1" == "supervisord" ]; then
     # create default server config if there is none
-    if [ ! -d "/data/DoNotStarveTogether" ]; then
+    if [ ! -d "${DST_USER_DATA_PATH}/DoNotStarveTogether" ]; then
         echo "Creating default server config..."
-        cp -r /opt/dst_default_config/* /data
-        touch /data/DoNotStarveTogether/Cluster_1/cluster_token.txt
-        chown -R dst:dst /data
-        echo "Done, please fill in \`DoNotStarveTogether/Cluster_1/cluster_token.txt\` with your cluster token and restart server!"
-        exit
+	mkdir -p "${DST_USER_DATA_PATH}"
+        cp -r /opt/dst_default_config/* "${DST_USER_DATA_PATH}"
+        touch "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt"
     fi
 
     # override server mods folder with user provided one
-    if [ ! -d "/data/DoNotStarveTogether/Cluster_1/mods" ]; then
+    if [ ! -d "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/mods" ]; then
         echo "Creating default mod config..."
-        mkdir -p /data/DoNotStarveTogether/Cluster_1
-        cp -r /opt/dst_server/mods /data/DoNotStarveTogether/Cluster_1
+        mkdir -p "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1"
+        cp -r /opt/dst_server/mods "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1"
     fi
     rm -rf /opt/dst_server/mods
-    ln -s /data/DoNotStarveTogether/Cluster_1/mods /opt/dst_server/mods
+    ln -s "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/mods" /opt/dst_server/mods
+
+    # fill cluster token from environment variable
+    if [ ! -z "${DST_CLUSTER_TOKEN:-}" ]; then
+	echo "Filling cluster token from environment variable"
+	printf "${DST_CLUSTER_TOKEN}" > "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt"
+    fi
 
     # check cluster token
-    if [ ! -f "/data/DoNotStarveTogether/Cluster_1/cluster_token.txt" ]; then
-        >&2 echo "cluster_token.txt not found!"
+    if [ ! -f "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt" ]; then
+        >&2 echo "Done, please fill in \`DoNotStarveTogether/Cluster_1/cluster_token.txt\` with your cluster token and restart server!"
+        exit
     else
         if [ -z "$(tail -c 1 "/data/DoNotStarveTogether/Cluster_1/cluster_token.txt")" ]; then
             # the cluster_token.txt needs to be terminated without newline, try to fix
-            mv /data/DoNotStarveTogether/Cluster_1/cluster_token.txt /tmp/
-            tr -d '\n' < /tmp/cluster_token.txt > /data/DoNotStarveTogether/Cluster_1/cluster_token.txt
+            mv "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt" /tmp/
+            tr -d '\n' < /tmp/cluster_token.txt > "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt"
             rm -f /tmp/cluster_token.txt
         fi
     fi
 
     # fix permission
-    chown -R dst:dst /data
+    chown -R "${DST_USER}:${DST_GROUP}" "${DST_USER_DATA_PATH}"
 
     # Update game
     echo "Updating server..."
     steamcmd +runscript /opt/steamcmd_scripts/install_dst_server
     echo "Updating mods..."
-    su - dst -c "dontstarve_dedicated_server_nullrenderer -persistent_storage_root /data -only_update_server_mods"
+    su --login --group "${DST_GROUP}" -c "dontstarve_dedicated_server_nullrenderer -persistent_storage_root \"${DST_USER_DATA_PATH}\" -only_update_server_mods" "${DST_USER}"
 
     # create unix socks server for supervisor
     touch /var/run/supervisor.sock
