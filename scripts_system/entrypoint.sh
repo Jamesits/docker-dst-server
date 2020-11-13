c819c7b61cfaaf8f6de7d83f10837ad76e26d3ce
James Swineson <github@public.swineson.me>
2020-11-13 21:42:54 +0800

    fix #34

--- scripts_system/entrypoint.sh
+++ scripts_system/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+DIR_MODS_SYS="/opt/dst_server/mods"
+DIR_MODS_USER="${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/mods"
+FILE_CLUSTER_TOKEN="${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt"
+
 # set -e error handler.
 on_error() {
     echo >&2 "Error on line ${1}${3+: ${3}}; RET ${2}."
@@ -9,7 +13,7 @@ on_error() {
 trap 'on_error ${LINENO} $?' ERR 2>/dev/null || true # some shells don't have ERR trap.
 
 if [ "$1" == "dontstarve_dedicated_server_nullrenderer" ] || [ "$1" == "supervisord" ]; then
-    # create default server config if there is none
+    # create a default server config if there is none
     if [ ! -d "${DST_USER_DATA_PATH}/DoNotStarveTogether" ]; then
         echo "Creating default server config..."
 	mkdir -p "${DST_USER_DATA_PATH}"
@@ -20,24 +24,31 @@ if [ "$1" == "dontstarve_dedicated_server_nullrenderer" ] || [ "$1" == "supervis
     # fill cluster token from environment variable
     if [ ! -z "${DST_CLUSTER_TOKEN:-}" ]; then
 	echo "Filling cluster token from environment variable"
-	printf "%s" "${DST_CLUSTER_TOKEN}" > "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt"
+	printf "%s" "${DST_CLUSTER_TOKEN}" > "${FILE_CLUSTER_TOKEN}"
     fi
 
-    # check cluster token
-    if [ ! -f "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt" ]; then
+    # check cluster token file format
+    if [ ! -f "${FILE_CLUSTER_TOKEN}" ]; then
         >&2 echo "Please fill in \`DoNotStarveTogether/Cluster_1/cluster_token.txt\` with your cluster token and restart server!"
         exit
     else
-        if [ -z "$(tail -c 1 "/data/DoNotStarveTogether/Cluster_1/cluster_token.txt")" ]; then
+        if [ -z "$(tail -c 1 "${FILE_CLUSTER_TOKEN}")" ]; then
             # the cluster_token.txt needs to be terminated without newline, try to fix
-            mv "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt" /tmp/
-            tr -d '\n' < /tmp/cluster_token.txt > "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/cluster_token.txt"
+            mv "${FILE_CLUSTER_TOKEN}" /tmp/cluster_token.txt
+            tr -d '\n' < /tmp/cluster_token.txt > "${FILE_CLUSTER_TOKEN}"
             rm -f /tmp/cluster_token.txt
         fi
     fi
 
-    # fix permission
+    # fix config file permission
     chown -R "${DST_USER}:${DST_GROUP}" "${DST_USER_DATA_PATH}"
+    
+    # protect our mods dir
+    # if the mods dir is already a symlink, then we temporary remove it to protect it, so that it survives a container restart
+    if [[ -L "${DIR_MODS_SYS}" ]]; then
+    	rm -f "${DIR_MODS_SYS}"
+	cp -r /opt/dst_default_config/DoNotStarveTogether/Cluster_1/mods "${DIR_MODS_SYS}"
+    fi
 
     # Update game
     # note that the update process modifies (resets) the mods folder so we symlink that later
@@ -45,24 +56,24 @@ if [ "$1" == "dontstarve_dedicated_server_nullrenderer" ] || [ "$1" == "supervis
     steamcmd +runscript /opt/steamcmd_scripts/install_dst_server
 
     # if there are no mods config, use the one that comes with the server
-    if [ ! -d "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/mods" ]; then
+    if [ ! -d "${DIR_MODS_USER}" ]; then
         echo "Creating default mod config..."
         mkdir -p "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1"
-        cp -r /opt/dst_server/mods "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1"
+        cp -r "${DIR_MODS_SYS}" "${DIR_MODS_USER}"
     fi
 
-    # override server mods folder with user provided one
-    rm -rf /opt/dst_server/mods
-    ln -s "${DST_USER_DATA_PATH}/DoNotStarveTogether/Cluster_1/mods" /opt/dst_server/mods
+    # override server mods folder with the user provided one
+    rm -rf "${DIR_MODS_SYS}"
+    ln -s "${DIR_MODS_USER}" "${DIR_MODS_SYS}"
 
     # update mods
     echo "Updating mods..."
     su --login --group "${DST_GROUP}" -c "dontstarve_dedicated_server_nullrenderer -persistent_storage_root \"${DST_USER_DATA_PATH}\" -only_update_server_mods" "${DST_USER}"
 
-    # remove any preexistent supervisor socket 
+    # remove any existing supervisor socket
     rm -f /var/run/supervisor.sock
     
-    # create unix socks server for supervisor
+    # create the unix socket file for supervisor
     touch /var/run/supervisor.sock
 fi
 
