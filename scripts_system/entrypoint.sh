ea0db57d97b8937ea43c97c52a6461b40413be20
James Swineson <github@public.swineson.me>
2021-10-29 21:46:18 +0800

    preserve env vars during mod update
    Reference: https://github.com/Jamesits/docker-dst-server/issues/21#issuecomment-954315527

--- scripts_system/entrypoint.sh
+++ scripts_system/entrypoint.sh
@@ -68,7 +68,7 @@ if [ "$1" == "dontstarve_dedicated_server_nullrenderer" ] || [ "$1" == "supervis
 
     # update mods
     echo "Updating mods..."
-    su --login --group "${DST_GROUP}" -c "dontstarve_dedicated_server_nullrenderer -persistent_storage_root \"${DST_USER_DATA_PATH}\" -ugc_directory \"${DST_USER_DATA_PATH}\"/ugc -only_update_server_mods" "${DST_USER}"
+    su --preserve-environment --group "${DST_GROUP}" -c "dontstarve_dedicated_server_nullrenderer -persistent_storage_root \"${DST_USER_DATA_PATH}\" -ugc_directory \"${DST_USER_DATA_PATH}\"/ugc -only_update_server_mods" "${DST_USER}"
 
     # remove any existing supervisor socket
     rm -f /var/run/supervisor.sock
