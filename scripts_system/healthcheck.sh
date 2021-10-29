7629e240b7fc4f86d19a3e80fd57ade42de6ca99
James Swineson <github@public.swineson.me>
2021-10-29 22:05:10 +0800

    fix healthcheck script early failure #45

--- scripts_system/healthcheck.sh
+++ scripts_system/healthcheck.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eu
+export LANGUAGE=C.UTF-8
 
-if [ "$(supervisorctl status | grep --invert-match --count RUNNING)" == 0 ]; then
+if ! supervisorctl status | grep --invert-match -E "(STARTING|RUNNING)"; then
     # if the 2 process is alive, assume everything's OK
     exit 0
 else
